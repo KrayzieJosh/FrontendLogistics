784cf4b9b6265f8a02fd991e4e7617ef548ef0af
Joshua Daniel Jacobs <114062249+KrayzieJosh@users.noreply.github.com>
2023-09-10 23:14:14 +0200

    added test code

--- app/src/main/java/za/ac/cput/MainActivity.java
+++ app/src/main/java/za/ac/cput/MainActivity.java
@@ -1,8 +1,13 @@
 package za.ac.cput;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import za.ac.cput.R;
+import za.ac.cput.view.LocationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openLocationButton = findViewById(R.id.openLocationButton);
+        openLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the LocationActivity when the button is clicked
+                Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
