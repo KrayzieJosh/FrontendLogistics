f32d35b16a281684ac309fdbc10b64cc36176b78
Joshua Daniel Jacobs <114062249+KrayzieJosh@users.noreply.github.com>
2023-09-14 22:19:43 +0200

    commenented entire location class, added volley to gradle , gave internet permissions to manifest

--- app/src/main/java/za/ac/cput/view/LocationActivity.java
+++ app/src/main/java/za/ac/cput/view/LocationActivity.java
@@ -5,8 +5,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import za.ac.cput.R;
-import za.ac.cput.domain.Location; // imports from backend not working !!
-import za.ac.cput.factory.LocationFactory;
+// import za.ac.cput.domain.Location; // imports from backend not working !!
+// import za.ac.cput.factory.LocationFactory;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -16,28 +16,28 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
 
         // Create a Location object using the factory
-        Location location = LocationFactory.createLocation(
-                null, // Leave locationId as null to generate it
-                "My Location",
-                123,
-                "Main Street",
-                "Cityville",
-                12345
-        );
+       // Location location = LocationFactory.createLocation(
+        //        null, // Leave locationId as null to generate it
+       //         "My Location",
+       //         123,
+       //         "Main Street",
+       //         "Cityville",
+        //        12345
+       // );
 
         // Find the TextView in your XML layout
-        TextView textView = findViewById(R.id.locationTextView);
+        //TextView textView = findViewById(R.id.locationTextView);
 
         // Display the location details in the TextView
-        if (location != null) {
-            String locationDetails = "Location ID: " + location.getLocationId() + "\n" +
-                    "Name: " + location.getName() + "\n" +
-                    "Street Number: " + location.getStreetNumber() + "\n" +
-                    "Street Name: " + location.getStreetName() + "\n" +
-                    "Town or City: " + location.getTownOrCity() + "\n" +
-                    "Area Code: " + location.getAreaCode();
+        //if (location != null) {
+        //    String locationDetails = "Location ID: " + location.getLocationId() + "\n" +
+        //            "Name: " + location.getName() + "\n" +
+        //            "Street Number: " + location.getStreetNumber() + "\n" +
+        //            "Street Name: " + location.getStreetName() + "\n" +
+        //            "Town or City: " + location.getTownOrCity() + "\n" +
+        //            "Area Code: " + location.getAreaCode();
 
-            textView.setText(locationDetails);
-        }
+        //    textView.setText(locationDetails);
+      //  }
     }
 }
